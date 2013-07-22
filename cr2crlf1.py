71e7568835201335708ab36e8056a2510a23afee
ftutor <csfangym@gmail.com>
2013-07-22 23:56:36 +0800

    convert '\r' to '\n' using three methods

--- cr2crlf1.py
+++ cr2crlf1.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python
 from __future__ import with_statement
 import os
-dir='/home/yingming.fang/project/assetcommander_git_repo/product/schema/'
-#dir ='C:\\Yingming Fang\\project\\Python\\gitPython\\'
+#dir='/home/yingming.fang/project/assetcommander_git_repo/product/schema/'
+dir ='C:\\Yingming Fang\\project\\Python\\gitPython\\'
 files = os.listdir(dir)
 
 
 for file in files:
-	if file.startswith('.')==False and file.endswith('Ui')==True:
+	if file.startswith('.')==False and file.endswith('.txt')==True:
 		filename = dir+file
 		output = dir+file
 		print filename
 		with open(filename, "rb") as f:
-			s = f.read().replace('\r\n', '\n').replace('\r', '\n')
+			s = f.read().replace('\r', '\n')
+			print "s::%s" %(s)
 
 		with open(output,"wb") as out:
 			out.write(s)
